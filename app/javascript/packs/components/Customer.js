b7b36c35f64ad4d4603f688b68b5a56e659a5a5b
shafrazi <shafrazi@live.com>
2021-03-31 22:39:40 +0530

    add calendar field to follow up form

--- app/javascript/packs/components/Customer.js
+++ app/javascript/packs/components/Customer.js
@@ -14,6 +14,7 @@ import {
   TableHead,
   TableBody,
   Button,
+  Grid,
 } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => {
@@ -32,6 +33,7 @@ function Customer() {
   const [dataChanged, setDataChanged] = useState(false);
   const [error, setError] = useState(null);
   const [correspondences, setCorrespondences] = useState([]);
+  const [followUps, setFollowUps] = useState([]);
   const classes = useStyles();
 
   axios.defaults.headers.common["X-CSRF-TOKEN"] = document.getElementsByName(
@@ -44,6 +46,7 @@ function Customer() {
       .then((response) => {
         setCustomerData(response.data.data);
         setCorrespondences(response.data.data.attributes.correspondences);
+        setFollowUps(response.data.data.attributes.follow_ups);
         setDataChanged(false);
       })
       .catch((error) => {
@@ -64,88 +67,137 @@ function Customer() {
 
   return (
     <div>
-      <Card>
-        <CardContent>
-          <Title>Customer Details</Title>
-          {customerData ? (
-            <form className={classes.root}>
-              <div>
-                <TextField
-                  name="first_name"
-                  label="First name"
-                  value={customerData.attributes.first_name}
-                  variant="outlined"
-                  readOnly={true}
-                />
-                <TextField
-                  name="last_name"
-                  label="Last name"
-                  value={customerData.attributes.last_name}
-                  variant="outlined"
-                  readOnly={true}
-                />
-              </div>
-              <div>
-                <TextField
-                  name="uid"
-                  label="UID"
-                  value={customerData.attributes.uid}
-                  variant="outlined"
-                  readOnly={true}
-                />
-                <TextField
-                  name="status"
-                  label="Current NPA status"
-                  value={customerData.attributes.status}
-                  variant="outlined"
-                  readOnly={true}
-                />
-              </div>
-            </form>
-          ) : (
-            error
-          )}
+      <Grid container spacing={3}>
+        <Grid item md={6}>
+          <Card>
+            <CardContent>
+              <Title>Customer Details</Title>
+              {customerData ? (
+                <form className={classes.root}>
+                  <div>
+                    <TextField
+                      name="first_name"
+                      label="First name"
+                      value={customerData.attributes.first_name}
+                      variant="outlined"
+                      readOnly={true}
+                    />
+                    <TextField
+                      name="last_name"
+                      label="Last name"
+                      value={customerData.attributes.last_name}
+                      variant="outlined"
+                      readOnly={true}
+                    />
+                  </div>
+                  <div>
+                    <TextField
+                      name="uid"
+                      label="UID"
+                      value={customerData.attributes.uid}
+                      variant="outlined"
+                      readOnly={true}
+                    />
+                    <TextField
+                      name="status"
+                      label="Current NPA status"
+                      value={customerData.attributes.status}
+                      variant="outlined"
+                      readOnly={true}
+                    />
+                  </div>
+                </form>
+              ) : (
+                error
+              )}
 
-          {correspondences.length > 0 && (
-            <div>
-              <Title>Correspondence history</Title>
-              <Table size="small">
-                <TableHead>
-                  <TableRow>
-                    <TableCell>Title</TableCell>
-                    <TableCell>Date</TableCell>
-                    <TableCell>Remarks</TableCell>
-                    <TableCell>Added by</TableCell>
-                    <TableCell></TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {correspondences.map((correspondence) => (
-                    <TableRow key={correspondence.id}>
-                      <TableCell>{correspondence.title}</TableCell>
-                      <TableCell>{correspondence.date}</TableCell>
-                      <TableCell>{correspondence.remarks}</TableCell>
-                      <TableCell>{correspondence.username}</TableCell>
-                      <TableCell>
-                        <Button
-                          variant="contained"
-                          color="secondary"
-                          size="small"
-                          onClick={() => {
-                            deleteCorrespondence(correspondence);
-                          }}
-                        >
-                          Delete
-                        </Button>
-                      </TableCell>
-                    </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
-            </div>
-          )}
-        </CardContent>
-      </Card>
+              {correspondences.length > 0 && (
+                <div>
+                  <Title>Correspondence history</Title>
+                  <Table size="small">
+                    <TableHead>
+                      <TableRow>
+                        <TableCell>Title</TableCell>
+                        <TableCell>Date</TableCell>
+                        <TableCell>Remarks</TableCell>
+                        <TableCell>Added by</TableCell>
+                        <TableCell></TableCell>
+                      </TableRow>
+                    </TableHead>
+                    <TableBody>
+                      {correspondences.map((correspondence) => (
+                        <TableRow key={correspondence.id}>
+                          <TableCell>{correspondence.title}</TableCell>
+                          <TableCell>{correspondence.date}</TableCell>
+                          <TableCell>{correspondence.remarks}</TableCell>
+                          <TableCell>{correspondence.username}</TableCell>
+                          <TableCell>
+                            <Button
+                              variant="contained"
+                              color="secondary"
+                              size="small"
+                              onClick={() => {
+                                deleteCorrespondence(correspondence);
+                              }}
+                            >
+                              Delete
+                            </Button>
+                          </TableCell>
+                        </TableRow>
+                      ))}
+                    </TableBody>
+                  </Table>
+                </div>
+              )}
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item md={6}>
+          <Card>
+            <CardContent>
+              <Title>Follow ups</Title>
+
+              {followUps.length > 0 && (
+                <div>
+                  <Table size="small">
+                    <TableHead>
+                      <TableRow>
+                        <TableCell>Description</TableCell>
+                        <TableCell>Date</TableCell>
+
+                        <TableCell>Added by</TableCell>
+                        <TableCell></TableCell>
+                      </TableRow>
+                    </TableHead>
+                    <TableBody>
+                      {followUps.map((followUp) => (
+                        <TableRow key={followUp.id}>
+                          <TableCell>{followUp.description}</TableCell>
+                          <TableCell>{followUp.date}</TableCell>
+
+                          <TableCell>{followUp.username}</TableCell>
+                          <TableCell>
+                            <Button
+                              variant="contained"
+                              color="secondary"
+                              size="small"
+                              onClick={() => {
+                                deleteCorrespondence(correspondence);
+                              }}
+                            >
+                              Delete
+                            </Button>
+                          </TableCell>
+                        </TableRow>
+                      ))}
+                    </TableBody>
+                  </Table>
+                </div>
+              )}
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
     </div>
   );
 }
--- app/serializers/customer_serializer.rb
+++ app/serializers/customer_serializer.rb
@@ -1,4 +1,4 @@
 class CustomerSerializer
   include JSONAPI::Serializer
-  attributes :uid, :first_name, :last_name, :status, :correspondences
+  attributes :uid, :first_name, :last_name, :status, :correspondences, :follow_ups
 end
