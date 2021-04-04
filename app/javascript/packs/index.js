8cc267fc916f766c130d93c6767ac65fc297f2ce
shafrazi <shafrazi@live.com>
2021-04-04 17:19:50 +0530

    add react-calendar css to index.js

--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "react-calendar/dist/Calendar.css";
 import { HashRouter } from "react-router-dom";
 import App from "./App";
 import { NpaContextProvider } from "./Context";
